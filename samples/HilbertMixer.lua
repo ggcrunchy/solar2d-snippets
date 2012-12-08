a0d9a579f45017ab23170629fe01bf1eaa6b5130
Steven Johnson <steve@xibalbastudios.com>
2012-12-08 17:10:20 -0600

    Curve option for Hilbert mixer polygon

--- samples/HilbertMixer.lua
+++ samples/HilbertMixer.lua
@@ -23,8 +23,12 @@
 -- [ MIT license: http://www.opensource.org/licenses/mit-license.php ]
 --
 
+-- Standard library imports --
+local abs = math.abs
+
 -- Modules --
 local buttons = require("ui.Button")
+local common = require("editor.Common")
 local hilbert = require("fill.Hilbert")
 local numeric_ops = require("numeric_ops")
 local scenes = require("game.Scenes")
@@ -54,6 +58,71 @@ end
 local Min = numeric_ops.RoundTo(2^6 * .2)
 local Max = numeric_ops.RoundTo(2^6 * .8)
 
+-- --
+local NParts = 5
+
+-- --
+local A = .25
+local B = 1 - A
+local H = .7
+
+--
+local function DoCurve (view, points, state, j, is_checked)
+	--
+	for i = 1, j, 2 do
+		local x1, y1 = points[i + 0], points[i + 1]
+		local x2, y2 = points[i + 2], points[i + 3]
+		local X, Y, x3, y3, dx, dy, d2
+
+		if is_checked and i ~= j then -- TODO: i == j case: still jagged at first point, I think
+			x3, y3 = points[i + 4], points[i + 5]
+
+			if x3 then
+				X, Y = x2, y2
+				x3, y3 = x2 + (x3 - x2) * A, y2 + (y3 - y2) * A
+				x2, y2 = x1 + (x2 - x1) * B, y1 + (y2 - y1) * B
+				dx, dy = x3 - x2, y3 - y2
+
+				if abs(dx) > 5 or abs(dy) > 5 then
+					d2 = dx * dx + dy * dy
+				else
+					x2, y2 = X, Y
+				end
+			end
+		end
+
+		if i == 1 then
+			state.polygon = display.newLine(view, x1, y1, x2, y2)
+
+			state.polygon.width = 3
+
+			state.polygon:setColor(is_checked and 0 or 255, is_checked and 255 or 0, 0)
+
+		else
+			state.polygon:append(x2, y2)
+		end
+
+		if d2 then
+			local t = ((X - x2) * dx + (Y - y2) * dy) / d2
+			local px, py = x2 + dx * t, y2 + dy * t
+			local nx, ny = X - px, Y - py
+
+			for k = 1, NParts do
+				t = k / NParts
+
+				local xc, yc = x2 + dx * t, y2 + dy * t
+
+				t = 2 * t - 1
+				t = H * (1 - t * t)
+
+				state.polygon:append(xc + nx * t, yc + ny * t)
+			end
+		end
+	end
+end
+
+local tt = {}
+
 --
 function Scene:enterScene ()
 	local points = {}
@@ -64,44 +133,49 @@ function Scene:enterScene ()
 	end
 
 	self.points = points
+	self.smooth = common.CheckboxWithText(self.view, 20, display.contentHeight - 70, "Smooth polygon")
 	self.t = timers.RepeatEx(function(event)
 		display.remove(self.polygon)
 
 		--
-		local now, n, cx, cy = event.m_elapsed, 0, 0, 0
-		local x0, y0 = GetXY(points[1] + now, points[2])
+		local now, j, n, cx, cy = event.m_elapsed, -1, 0, 0, 0
 
 		for i = 1, #points, 2 do
-			n = n + 1
+			j, n = j + 2, n + 1
 
 			local x, y = GetXY(points[i] + now, points[i + 1])
 
-			if n > 2 then
-				self.polygon:append(x, y)
-			elseif n == 2 then
-				self.polygon = display.newLine(self.view, x0, y0, x, y)
-
-				self.polygon.width = 3
-
-				self.polygon:setColor(255, 0, 0)
-			end
+			self[j], self[j + 1] = x, y
 
 			cx, cy = cx + x, cy + y
 		end
 
-		self.polygon:append(x0, y0)
+		self[j + 2], self[j + 3] = self[1], self[2]
 
 		--
-		cx, cy = cx / n, cy / n
+		local is_checked = self.smooth:IsChecked()
+--[[
+		display.remove(tt.polygon)
 
+		tt.polygon = nil
+
+		if is_checked then
+			DoCurve(self.view, self, tt, j, true)
+		end
+]]
+		DoCurve(self.view, self, self, j, is_checked)
+
+		--
 		if not self.trace then
 			self.trace = display.newCircle(self.view, 0, 0, 10)
 			
 			self.trace:setFillColor(0, 0, 255)
 		end
 
-		self.trace.x, self.trace.y = cx, cy
-	end, 70)
+		self.trace.x, self.trace.y = cx / n, cy / n
+	end)
+
+	self.smooth.isVisible = true
 end
 
 Scene:addEventListener("enterScene")
@@ -110,15 +184,16 @@ Scene:addEventListener("enterScene")
 function Scene:exitScene ()
 	timer.cancel(self.t)
 
+	self.points = nil
 	self.t = nil
 
 	display.remove(self.polygon)
+	display.remove(self.smooth)
 	display.remove(self.trace)
 
-	self.points = nil
 	self.polygon = nil
+	self.smooth = nil
 	self.trace = nil
-	self.text = nil
 end
 
 Scene:addEventListener("exitScene")
