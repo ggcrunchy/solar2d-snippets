b623c49082d6dbafe1b7bb1fcb96593a52936876
Steven Johnson <steve@xibalbastudios.com>
2013-12-28 17:23:21 -0600

    Curves demo deleted (the interesting bits were assimilated into Tiling); tiling demo much improved: more interesting fade-in effect, alternating between effects on the tiles, as well as methods of showing those same effects; scene-related comments removed from choices menu (now in the project wiki)

--- samples/Tiling.lua
+++ samples/Tiling.lua
@@ -24,19 +24,27 @@
 --
 
 -- Standard library imports --
-local floor = math.floor
+local ceil = math.ceil
+local ipairs = ipairs
 local random = math.random
 local yield = coroutine.yield
 
 -- Modules --
 local buttons = require("ui.Button")
 local ca = require("fill.CellularAutomata")
+local circle = require("fill.Circle")
+local curves = require("utils.Curves")
+local flow_ops = require("flow_ops")
+local grid_iterators = require("grid_iterators")
+local index_ops = require("index_ops")
+local numeric_ops = require("numeric_ops")
 local scenes = require("utils.Scenes")
 local sheet = require("ui.Sheet")
 local timers = require("game.Timers")
 
 -- Corona globals --
 local display = display
+local native = native
 local timer = timer
 local transition = transition
 
@@ -49,110 +57,318 @@ local Scene = storyboard.newScene()
 --
 function Scene:createScene ()
 	buttons.Button(self.view, nil, 120, 75, 200, 50, scenes.Opener{ name = "scene.Choices" }, "Go Back")
+
+	self.action = display.newText(self.view, "", 250, 35, native.systemFontBold, 24)
+	self.effect = display.newText(self.view, "", 250, 65, native.systemFontBold, 24)
+
+	self.action.anchorX = 0
+	self.effect.anchorX = 0
 end
 
 Scene:addEventListener("createScene")
 
--- Heh, not exactly the most efficient representation :P
-local function Index (col, row, pitch)
-	local qc, rc = floor((col - 1) / 4), (col - 1) % 4
-	local qr, rr = floor((row - 1) / 4), (row - 1) % 4
+-- --
+local NCols_Log, NRows_Log = 15, 10
 
-	return 4 * (qr * pitch + qc * 4 + rr) + rc + 1
+-- --
+local NCols, NRows = NCols_Log * 4, NRows_Log * 4
+
+-- --
+local Pitch = NCols_Log * 4
+
+-- --
+local X, Y = 150, 150
+
+-- --
+local LogicalDim = 32
+
+-- --
+local TileDim = LogicalDim / 4
+
+-- Heh, not exactly the most efficient representation :P
+local function Index (col, row)
+	local qc, rc = numeric_ops.DivRem(col - 1, 4)
+	local qr, rr = numeric_ops.DivRem(row - 1, 4)
+
+	return 4 * (qr * Pitch + qc * 4 + rr) + rc + 1
+end
+
+--
+local function TileCoords (x, y)
+	local col = index_ops.FitToSlot(x, 0, TileDim)
+	local row = index_ops.FitToSlot(y, 0, TileDim)
+
+	return col, row
 end
 
 -- --
 local A, B, C
 
 --
-function A (tiles, pitch)
-	-- Curves
-	return B(tiles, pitch)
+local function SetupCurve (curve)
+	local radius = curve.radius
+
+	curve.x, curve.rx = random(10, NCols - 10) * TileDim, random() < .5 and radius or -radius
+	curve.y, curve.ry = random(10, NRows - 10) * TileDim, random() < .5 and radius or -radius
+end
+
+-- --
+local Curves = {
+	{ func = curves.Tschirnhausen, radius = 15 },
+	{ func = curves.SingularCubic, radius = 200 },
+	{ func = curves.Tschirnhausen, radius = 35 }
+}
+
+-- --
+local SetTo
+
+-- --
+local Ops = {
+	--
+	function(tile, set)
+		if tile then
+			tile.alpha = set and SetTo or 1
+		else
+			SetTo = .55 + random() * .35
+			Scene.effect.text = ("Effect: alpha = %.3f"):format(SetTo)
+		end
+	end,
+
+	--
+	function(tile, set)
+		if tile then
+			tile.rotation = set and SetTo or 0
+		else
+			SetTo = random(10, 60)
+			Scene.effect.text = ("Effect: rotation = %i"):format(SetTo)
+		end
+	end
+-- TODO: marching squares masks (mostly working, needs minor formalization and a port... also, somewhat different interface)
+} 
+
+-- --
+local function Mark (tiles, index, op, set)
+	Ops[op](tiles[index], set)
+end
+
+-- --
+local Dirty, DirtyN = {}, 0
+
+-- --
+local function CleanUp (tiles, op)
+	op = Ops[op]
+
+	for i = 1, DirtyN do
+		op(tiles[Dirty[i]], false)
+	end
+
+	DirtyN = 0
 end
 
 --
-function B (tiles, pitch)
-	-- Ripples
-	return C(tiles, pitch)
+local function Mark_Add (tiles, col, row, op)
+	if col >= 1 and col <= NCols and row >= 1 and row <= NRows then
+		local index = Index(col, row)
+
+		Mark(tiles, index, op, true)
+
+		Dirty[DirtyN + 1], DirtyN = index, DirtyN + 1
+	end
+end
+
+--
+function A (tiles, op)
+	Scene.action.text = "Action: drawing curves"
+
+	--
+	op = index_ops.RotateIndex(op, #Ops)
+
+	Ops[op]()
+
+	--
+	for _ = 1, 5 do
+		for _, curve in ipairs(Curves) do
+			SetupCurve(curve)
+		end
+
+		local t1, nsteps = 0, 20
+
+		for i = 1, nsteps do
+			local t2 = i / nsteps
+
+			for _, curve in ipairs(Curves) do
+				local x, rx = curve.x, curve.rx
+				local y, ry = curve.y, curve.ry
+				local x1, y1 = curve.func(t1)
+				local x2, y2 = curve.func(t2)
+				local c1, r1 = TileCoords(x + x1 * rx, y + y1 * ry)
+				local c2, r2 = TileCoords(x + x2 * rx, y + y2 * ry)
+
+				for col, row in grid_iterators.LineIter(c1, r1, c2, r2) do
+					Mark_Add(tiles, col, row, op)
+				end
+			end
+
+			t1 = t2
+
+			yield()
+		end
+
+		flow_ops.Wait(.85)
+
+		CleanUp(tiles, op)
+	end
+
+	return B(tiles, op)
+end
+
+--
+function B (tiles, op)
+	Scene.action.text = "Action: drawing ripples"
+
+	--
+	local ripples = {}
+
+	for i = 1, 60 do
+		local col = random(10, NCols - 10)
+		local row = random(10, NRows - 10)
+		local iters, wait = random(10, 15), random(0, 50)
+		local radius, inc = 1, .23 * iters / (20 - iters)
+
+		--
+		local spread = circle.SpreadOut(7, 7, function(x, y, _)
+			Mark_Add(tiles, col + x, row + y, op)
+		end)
+
+		--
+		ripples[i] = function()
+			if wait > 0 then
+				wait = wait - 1
+			else
+				radius, iters = radius + inc, iters - 1
+
+				spread(ceil(radius))
+
+				return iters == 0
+			end
+		end
+	end
+
+	--
+	repeat
+		local n = #ripples
+
+		for i = n, 1, -1 do
+			if ripples[i]() then
+				ripples[i] = ripples[n]
+				n, ripples[n] = n - 1
+			end
+		end
+
+		yield()
+
+		CleanUp(tiles, op)
+	until n == 0
+
+	return C(tiles, op)
 end
 
 -- --
 local CA
 
 --
-function C (tiles, pitch)
-	CA = CA or ca.GosperGliderGun(10, 10, 8, 8, function(_, _, set, col, row)
-		tiles[Index(col, row, pitch)].alpha = set and .9 or 1
-	end)
+function C (tiles, op)
+	Scene.action.text = "Action: drawing Gosper's glider gun"
 
-	while true do
-		CA("update")
+	--
+	CA = CA or ca.GosperGliderGun(10, 10, 8, 8, function(how, _, set, col, row, op)
+		if how == "visit" then
+			set = false
+		end
+
+		Mark(tiles, Index(col, row), op, set)
+	end, op)
+
+	--
+	for _ = 1, 150 do
+		CA("update", op)
+
 		yield()
 	end
 
-	-- Toggle mode?
+	CA("visit", op)
 
-	return A(tiles, pitch)
+	return A(tiles, op)
 end
 
 -- --
-local FadeInParams = { time = 700, alpha = 1, transition = easing.outQuad }
+local NumLeft
+
+-- --
+local FadeInParams = {
+	time = 400, alpha = 1, transition = easing.outQuad,
+
+	onComplete = function()
+		NumLeft = NumLeft - 1
+	end
+}
 
 --
 function Scene:enterScene ()
-	local ncols, nrows = 15, 10
-	local images = sheet.TileImage("Background_Assets/Background.png", ncols * 4, nrows * 4, 120, 80, 330, 200)
+	local images = sheet.TileImage("Background_Assets/Background.png", NCols, NRows, 120, 80, 330, 200)
 
 	self.tiles = display.newGroup()
 
 	self.view:insert(self.tiles)
 
-	local index, row, col = 1, 0, 0
-	local dim, tdim, pitch = 32, 8, ncols * 4
+	NumLeft = NCols * NRows
 
-	self.timer = timer.performWithDelay(80, function()
-		local x = 150 + col * dim
-		local y = 150 + row * dim
+	local index, row, col = 1, 0, 0
+
+	self.action.text = "Action: filling tiles"
+
+	self.timer = timer.performWithDelay(40, function()
+		local x = X + col * LogicalDim
+		local y = Y + row * LogicalDim
 		local di, delay = 0, 0
 
 		for dr = 0, 3 do
 			for dc = 0, 3 do
 				local tile = display.newImage(self.tiles, images, index + di + dc)
 
-				tile.xScale = tdim / tile.width
-				tile.yScale = tdim / tile.height
-				tile.anchorX, tile.x = 0, x + dc * tdim
-				tile.anchorY, tile.y = 0, y + dr * tdim
+				tile.xScale = TileDim / tile.width
+				tile.yScale = TileDim / tile.height
+				tile.anchorX, tile.x = 0, x + dc * TileDim
+				tile.anchorY, tile.y = 0, y + dr * TileDim
 				tile.alpha = .2
 
 				FadeInParams.delay = delay
 
 				transition.to(tile, FadeInParams)
 
-				delay = delay + random(180, 300)
+				delay = delay + random(120, 250)
 			end
 
-			di = di + pitch
+			di = di + Pitch
 		end
 
 		col, index = col + 1, index + 4
 
-		if col == ncols then
+		if col == NCols_Log then
 			col, row = 0, row + 1
-			index = index + pitch * 3
+			index = index + Pitch * 3
 
-			if row == nrows then
+			if row == NRows_Log then
 				self.effects = timers.Wrap(function()
-					return A(self.tiles, pitch)
+					while NumLeft > 0 do
+						yield()
+					end
+
+					return A(self.tiles, 0)
 				end, 60)
 			end
 		end
-	end, ncols * nrows)
-	-- ^ TODO: and then, fancy effects... ripples, streamers, curves?
-	-- Absorb curves demo, do those (utils.Curves, grid_iterators)
-	-- Then some ripples (fill.Circle)
-	-- Then some cellular automata (TODO, just inlined)
-	-- Toggle between alpha (and scale?) mode / marching squares masks (mostly working, needs minor formalization and a port)
+	end, NCols_Log * NRows_Log)
 end
 
 Scene:addEventListener("enterScene")
@@ -170,6 +386,9 @@ function Scene:exitScene ()
 	self.effects = nil
 	self.tiles = nil
 	self.timer = nil
+
+	self.action.text = ""
+	self.effect.text = ""
 
 	CA = nil
 end
--- scene/Choices.lua
+++ scene/Choices.lua
@@ -1,4 +1,6 @@
----
+--- The samples scene.
+--
+-- Consult the wiki for more details.
 
 --
 -- Permission is hereby granted, free of charge, to any person obtaining
@@ -47,94 +49,28 @@ end
 local Scene = storyboard.newScene()
 
 -- Samples names --
+-- Various notes
 local Names = {
-	-- "Corridor", -- as alluded in "Pixels" and note further down, some kind of corridor scrolling
-	"Curves", -- v 0.1 (N.B. HOTFIXED): These were mostly just for testing something... could be MUCH more interesting... (editor? splines?)
-	"Delaunay", -- v 0.3: Much of build process working... need to work out the mesh ops to show more
-	-- "Fire", -- Just hashing out the basic idea so far
-	"Hilbert", -- v 1.0 (N.B. HOTFIXED): I rather like this, nothing immediate to add...
-	"HilbertMixer", -- v 0.9: Getting there. Do smoothing or simplification?
-	"Hop", -- v 0.9: Was just to show an idea to a work colleague... has some "problems"... worth fixing?
-	"Marching", -- v 0.4: Better example? Better implementation? (Not really very general / adaptable)
-	"Nodes", -- v 0.8: Was incorporated into editor... though not quite like this... at very least, use LinkGroup?
-	"OrbitsAndLattices", -- v 0.8: In progress... Probably mostly needs some blending, transitions, etc.
-	"Pixels", -- v 0.9: Depending on how far I can squeeze this, look into a "3D engine" on top (actually, move that elsewhere and do fire, plasma, etc. here)
-	"Plasma", -- v 0.2: Very basic, just trying to get something up and running
-	-- "Ray Tracer", -- Some sort of scene, Hilbert-iterated, save / load sessions?
-	"SlowMo", -- v 0.9: Explore the "copycat" idea I have here?
-	"Snowfall", -- v 0.3: In progress... Already in, e.g. CBEffects... anyhow, see if adapting the (abandoned) Icebreakers stuff would look decent for ash, leaves, snow, etc.
-	"Superformulae", -- v 0.8: Getting there... is there any way to analyze the formula? (can get REALLY huge...)
-	"Thoughts", -- v 0.6: Seems to need some fixing...
-	"Ticker", -- v 0.1: Get something started...
-	"Tiling", -- v 0.5: Do effects on these, once in play?
-	"Timers", -- v 0.3: Better examples?
-	"Game", -- v 0.7: Having actual game ideas... explore? Which are best?
-	"Editor" -- v 0.6: Ongoing (fairly easy to port now, owing to config directory)
+	"Delaunay",
+	-- "Fire",
+	"Hilbert",
+	"HilbertMixer",
+	"Hop",
+	"Marching",
+	"Nodes",
+	"OrbitsAndLattices",
+	"Pixels",
+	"Plasma",
+	"SlowMo",
+	"Snowfall",
+	"Superformulae",
+	"Thoughts",
+	"Ticker",
+	"Tiling",
+	"Timers",
+	"Game",
+	"Editor"
 }
-
---[[
-	This seems a decent staging area for new ideas, versus the "TESTING" scene used now
-	in-game.
-
-	Prospects for future additions:
-
-	* Ramer-Douglas-Peucker algorithm (note: saw that somebody else had this, possibly on the Code Exchange)
-
-		function DouglasPeucker(PointList[], epsilon)
-			//Find the point with the maximum distance
-			dmax = 0
-			index = 0
-			for i = 2 to (length(PointList) - 1)
-				d = PerpendicularDistance(PointList[i], Line(PointList[1], PointList[end])) 
-				if d > dmax
-					index = i
-					dmax = d
-				end
-			end
-
-			//If max distance is greater than epsilon, recursively simplify
-			if dmax >= epsilon
-				//Recursive call
-				recResults1[] = DouglasPeucker(PointList[1...index], epsilon)
-				recResults2[] = DouglasPeucker(PointList[index...end], epsilon)
-
-				// Build the result list
-				ResultList[] = {recResults1[1...end-1] recResults2[1...end]}
-			else
-				ResultList[] = {PointList[1], PointList[end]}
-			end
-
-			//Return the result
-			return ResultList[]
-		end
-
-		Also try Lang simplification, McMaster's slide, etc.
-
-		http://psimpl.sourceforge.net/reumann-witkam.html
-		http://www.motiondraw.com/blog/?p=50
-
-	* Play with some priority queues (sort of alluded to in Tektite wiki)
-
-	* Timeline / ladder queue stuff? (ditto... need to reread latter paper, pseudo-code not so helpful :P)
-
-	* Theta* or Phi* demo (not sure off-hand if both would be redundant) (Already done in Jumper?)
-
-	* Approximate crumble effect from Vektor Space / Icebreakers (need general quads to get it right...
-	  update: "general" quads upcoming in Graphics 2.0... not sure about texels)
-
-	* Go to town with the grid iterators, e.g. to make masks or general art... could amortize costs
-	  by iteratively capturing completed sections and compositing them? (Ditto, would be a lot easier
-	  with render textures)
-
-	* Coupled map lattices, logistic maps, etc.
-
-	* Projectile targeting, with gravity (maybe with some of the competing teams stuff from WAY back...)
-	  (Also came up on forums)
-
-	* Try to do 8- or 16-bit style of 3D, like World Runner or Panorama Cotton (proof of concept)
-
-	* Ray tracer
-]]
 
 -- --
 local CX
