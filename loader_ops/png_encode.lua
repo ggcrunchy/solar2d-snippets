a5467575f5272781f96aa6d7595ca03ffea32960
Steven Johnson <steve@xibalbastudios.com>
2014-03-04 23:33:03 -0600

    Tentative PNG saver, to be tested

--- loader_ops/png_encode.lua
+++ loader_ops/png_encode.lua
@@ -25,8 +25,16 @@
 
 -- Standard library imports --
 local byte = byte
+local ceil = math.ceil
 local char = string.char
+local concat = table.concat
+local floor = math.floor
 local gmatch = string.gmatch
+local ipairs = ipairs
+local open = io.open
+local max = math.max
+local min = math.min
+local unpack = unpack
 
 -- Modules --
 local operators = require("bitwise_ops.operators")
@@ -37,16 +45,88 @@ local bnot = operators.bnot
 local bxor = operators.bxor
 local rshift = operators.rshift
 
+-- Cached module references --
+local _ToString_Interleaved_
+local _ToString_RGBA_
+
 -- Exports --
 local M = {}
 
+--[[
+	From http://www.chrfr.de/software/midp_png.html:
+
+	/*
+	 * Minimal PNG encoder to create PNG streams (and MIDP images) from RGBA arrays.
+	 * 
+	 * Copyright 2006-2009 Christian Fröschlin 
+	 *
+	 * www.chrfr.de
+	 *
+	 *
+	 * Changelog:
+	 *
+	 * 09/22/08: Fixed Adler checksum calculation and byte order
+	 *           for storing length of zlib deflate block. Thanks
+	 *           to Miloslav Ruzicka for noting this.
+	 *
+	 * 05/12/09: Split PNG and ZLIB functionality into separate classes.
+	 *           Added support for images > 64K by splitting the data into
+	 *           multiple uncompressed deflate blocks.
+	 *
+	 * Terms of Use:  
+	 *
+	 * You may use the PNG encoder free of charge for any purpose you desire, as long
+	 * as you do not claim credit for the original sources and agree not to hold me
+	 * responsible for any damage arising out of its use.
+	 *
+	 * If you have a suitable location in GUI or documentation for giving credit,
+	 * I'd appreciate a mention of
+	 * 
+	 *  PNG encoder (C) 2006-2009 by Christian Fröschlin, www.chrfr.de
+	 *
+	 * but that's not mandatory.
+	 *
+	 */
+--]]
+
+--- DOCME
+function M.Save_Interleaved (name, colors, w, opts)
+	local str = _ToString_Interleaved_(colors, w, opts)
+	local file = open(name, "w")
+
+	if file then
+		file:write(str)
+		file:close()
+	end
+
+	return file ~= nil
+end
+
+--- DOCME
+function M.Save_RGBA (name, r, g, b, a, w, opts)
+	local str = _ToString_RGBA_(r, g, b, a, w, opts)
+	local file = open(name, "w")
+
+	if file then
+		file:write(str)
+		file:close()
+	end
+
+	return file ~= nil
+end
+
 --
-local function Adler (bytes)
+local function U32 (num)
+	local low1, low2, low3 = num % 2^8, num % 2^16, num % 2^24
+	
+	return char((num - low3) / 2^24, (low3 - low2) / 2^16, (low2 - low1) / 2^8, low1)
+end
+
+--
+local function Adler (data)
 	local s1, s2 = 1, 0
 
-	for b in gmatch(bytes, ".") do
-		b = byte(b)
-
+	for _, b in ipairs(data) do
 		local abs = b >= 0 and b or b + 256
 
 		s1 = (s1 + abs) % 65521
@@ -57,15 +137,40 @@ local function Adler (bytes)
 end
 
 --
-local function WriteU8 (stream, num)
-	stream[#stream + 1] = char(num)
+local function WriteUncompressedBlock (stream, is_last, data, offset, len)
+	local nlen = bnot(len)
+	local lenFF, nlenFF = band(len, 0xFF), band(nlen, 0xFF)
+
+	stream[#stream + 1] = char(is_last and 1 or 0) -- Final flag, Compression type 0
+	stream[#stream + 1] = char(lenFF) -- Length LSB
+	stream[#stream + 1] = char(rshift(len - lenFF), 8)) -- Length MSB
+	stream[#stream + 1] = char(nlenFF) -- Length 1st complement LSB
+	stream[#stream + 1] = char(rshift(nlen - nlenFF), 8)) -- Length 1st complement MSB
+	stream[#stream + 1] = char(unpack(data, offset + 1, offset + len)) -- Data
 end
 
+-- --
+local BlockSize = 32000
+
+-- --
+local Byte1 = char(8) -- CM = 8, CMINFO = 0
+local Byte2 = char((31 - (8 * 256) % 31) % 31) -- FCHECK (FDICT / FLEVEL = 0)
+
 --
-local function WriteU32 (stream, num)
-	local low1, low2, low3 = num % 2^8, num % 2^16, num % 2^24
-	
-	stream[#stream + 1] = char((num - low3) / 2^24, (low3 - low2) / 2^16, (low2 - low1) / 2^8, low1)
+local function UncompressedWrite (data)
+	local subs, pos, n = { Byte1, Byte2 }, 0, #data
+
+	while n - pos > BlockSize do
+		WriteUncompressedBlock(subs, false, data, pos, BlockSize)
+
+		pos = pos + BlockSize
+	end
+
+	WriteUncompressedBlock(subs, true, data, pos, n - pos)
+
+	subs[#subs + 1] = U32(Adler(data))
+
+	return concat(subs, "")
 end
 
 -- --
@@ -103,247 +208,98 @@ local function UpdateCRC (crc, bytes)
 	return crc
 end
 
---[[
-http://www.chrfr.de/software/midp_png.html
+--
+local function ToChunk (stream, id, bytes)
+	stream[#stream + 1] = U32(#bytes)
+	stream[#stream + 1] = id
+	stream[#stream + 1] = bytes
 
-/*
- * Minimal PNG encoder to create PNG streams (and MIDP images) from RGBA arrays.
- * 
- * Copyright 2006-2009 Christian Fröschlin 
- *
- * www.chrfr.de
- *
- *
- * Changelog:
- *
- * 09/22/08: Fixed Adler checksum calculation and byte order
- *           for storing length of zlib deflate block. Thanks
- *           to Miloslav Ruzicka for noting this.
- *
- * 05/12/09: Split PNG and ZLIB functionality into separate classes.
- *           Added support for images > 64K by splitting the data into
- *           multiple uncompressed deflate blocks.
- *
- * Terms of Use:  
- *
- * You may use the PNG encoder free of charge for any purpose you desire, as long
- * as you do not claim credit for the original sources and agree not to hold me
- * responsible for any damage arising out of its use.
- *
- * If you have a suitable location in GUI or documentation for giving credit,
- * I'd appreciate a mention of
- * 
- *  PNG encoder (C) 2006-2009 by Christian Fröschlin, www.chrfr.de
- *
- * but that's not mandatory.
- *
- */
+	local crc = 0xFFFFFFFF
 
-package com.wordpress.utils;
+	crc = UpdateCRC(crc, id)
+	crc = UpdateCRC(crc, bytes)
 
-import java.io.*;
+	stream[#stream + 1] = bnot(crc)
+end
 
-import javax.microedition.lcdui.Image;
+--
+local function Write (colors, w, h, to_zlib, yfunc)
+	local stream = { "\137\080\078\071\013\010\026\010" }
 
-import net.rim.device.api.compress.ZLibOutputStream;
+	ToChunk(stream, "IHDR", U32(w) .. U32(h) .. char(8, 6, 0, 0, 0) -- Bit depth, colortype (ARGB), compression, filter, interlace
+	ToChunk(stream, "IDAT", to_zlib(colors))
+	ToChunk(stream, "IEND", "")
 
-public class MinimalPNGEncoder
-{
-  public static Image toImage(int width, int height, byte[] alpha, byte[] red, byte[] green, byte[] blue)
-  {
-    try    
-    {
-      byte[] png = toPNG(width, height, alpha, red, green, blue);
-      return Image.createImage(png, 0, png.length);
-    }
-    catch (IOException e)
-    {
-      return null;
-    }
-  }
+	return concat(stream, "")
+end
 
-  public static byte[] toPNG(int width, int height, byte[] alpha, byte[] red, byte[] green, byte[] blue) throws IOException
-  {
-    byte[] signature = new byte[] {(byte) 137, (byte) 80, (byte) 78, (byte) 71, (byte) 13, (byte) 10, (byte) 26, (byte) 10};
-    byte[] header = createHeaderChunk(width, height);
-    byte[] data = createDataChunk(width, height, alpha, red, green, blue);
-    byte[] trailer = createTrailerChunk();
-    
-    ByteArrayOutputStream png = new ByteArrayOutputStream(signature.length + header.length + data.length + trailer.length);
-    png.write(signature);
-    png.write(header);
-    png.write(data);
-    png.write(trailer);
-    return png.toByteArray();
-  }
+--
+local function DefYieldFunc () end
 
-  public static byte[] createHeaderChunk(int width, int height) throws IOException
-  {
-    ByteArrayOutputStream baos = new ByteArrayOutputStream(13);
-    DataOutputStream chunk = new DataOutputStream(baos);
-    chunk.writeInt(width);
-    chunk.writeInt(height);
-    chunk.writeByte(8); // Bitdepth
-    chunk.writeByte(6); // Colortype ARGB
-    chunk.writeByte(0); // Compression
-    chunk.writeByte(0); // Filter
-    chunk.writeByte(0); // Interlace    
-    return toChunk("IHDR", baos.toByteArray());
-  }
-  
-  public static byte[] createDataChunk(int width, int height, byte[] alpha, byte[] red, byte[] green, byte[] blue) throws IOException
-  {    
-    int source = 0;
-    int dest = 0;
-    byte[] raw = new byte[4*(width*height) + height];
-    for (int y = 0; y < height; y++)
-    {
-      raw[dest++] = 0; // No filter
-      for (int x = 0; x < width; x++)
-      {
-        raw[dest++] = red[source];
-        raw[dest++] = green[source];
-        raw[dest++] = blue[source];
-        raw[dest++] = alpha[source++];
-      }
-    }
-    return toChunk("IDAT", toZLIB(raw));
-  }
+--- DOCME
+function M.ToString_Interleaved (colors, w, opts)
+	local ncolors = floor(#colors / 4)
+	local h, data = ceil(ncolors / w), {}
+	local si, di, extra = 1, 1, w * h - ncolors
+	local yfunc = (opts and opts.yfunc) or DefYieldFunc()
 
-  public static byte[] createTrailerChunk() throws IOException
-  {
-    return toChunk("IEND", new byte[] {});
-  }
-  
-  public static byte[] toChunk(String id, byte[] raw) throws IOException
-  {
-    ByteArrayOutputStream baos = new ByteArrayOutputStream(raw.length + 12);
-    DataOutputStream chunk = new DataOutputStream(baos);
-    
-    chunk.writeInt(raw.length);
-    
-    byte[] bid = new byte[4];
-    for (int i = 0; i < 4; i++)
-    {
-      bid[i] = (byte) id.charAt(i);
-    }
-    
-    chunk.write(bid);
-        
-    chunk.write(raw);
-    
-    int crc = 0xFFFFFFFF;
-    crc = updateCRC(crc, bid);  
-    crc = updateCRC(crc, raw);    
-    chunk.writeInt(~crc);
-    
-    return baos.toByteArray();
-  }
+	-- Inject filters and do a standard write.
+	repeat
+		data[di], di = 0, di + 1 -- No filter
 
-  static int[] crcTable = null;
-  
-  public static void createCRCTable()
-  {
-    crcTable = new int[256];
-    
-    for (int i = 0; i < 256; i++)
-    {
-      int c = i;
-      for (int k = 0; k < 8; k++)
-      {
-        c = ((c & 1) > 0) ? 0xedb88320 ^ (c >>> 1) : c >>> 1;
-      }
-      crcTable[i] = c;
-    }
-  }
-  
-  public static int updateCRC(int crc, byte[] raw)
-  {
-    if (crcTable == null)
-    {
-      createCRCTable();
-    }
-    
-    for (int i = 0; i < raw.length; i++)
-    {
-      crc = crcTable[(crc ^ raw[i]) & 0xFF] ^ (crc >>> 8);      
-    }
-    
-    return crc;
-  }
+		local count = min(w, ncolors)
 
-  /* This method is called to encode the image data as a zlib
-     block as required by the PNG specification. This file comes
-     with a minimal ZLIB encoder which uses uncompressed deflate
-     blocks (fast, short, easy, but no compression). If you want
-     compression, call another encoder (such as JZLib?) here. */
-  public static byte[] toZLIB(byte[] raw) throws IOException
-  {
-	  //used the BB ZLib ...
-	  ByteArrayOutputStream outBytes = new ByteArrayOutputStream(1024);
-      ZLibOutputStream compBytes = new ZLibOutputStream(outBytes, false, 10, 9);
-	  compBytes.write(raw, 0, raw.length);
-	  compBytes.close();
-	  return outBytes.toByteArray();
-	 //return ZLIB.toZLIB(raw);
-  }
-}
+		for _ = 1, count * 4 do
+			data[di], si, di = colors[si], si + 1, di + 1
+		end
 
+		ncolors = ncolors - count
 
+		yfunc()
+	until ncolors == 0
 
-class ZLIB
-{
-  static final int BLOCK_SIZE = 32000;
-  
-  public static byte[] toZLIB(byte[] raw) throws IOException
-  {    
-    ByteArrayOutputStream baos = new ByteArrayOutputStream(raw.length + 6 + (raw.length / BLOCK_SIZE) * 5);
-    DataOutputStream zlib = new DataOutputStream(baos);
-    
-    byte tmp = (byte) 8;       
-    zlib.writeByte(tmp);                           // CM = 8, CMINFO = 0
-    zlib.writeByte((31 - ((tmp << 8) % 31)) % 31); // FCHECK (FDICT/FLEVEL=0)
+	for _ = 1, extra do
+		data[di], data[di + 1], data[di + 2], data[di + 3] = 0, 0, 0, 0
+		di = di + 4
+	end
 
-    int pos = 0;
-    while (raw.length - pos > BLOCK_SIZE)
-    {
-      writeUncompressedDeflateBlock(zlib, false, raw, pos, (char) BLOCK_SIZE);
-      pos += BLOCK_SIZE;
-    }
-    
-    writeUncompressedDeflateBlock(zlib, true, raw, pos, (char) (raw.length - pos));
-        
-    // zlib check sum of uncompressed data
-    zlib.writeInt(calcADLER32(raw));
+	yfunc()
 
-    return baos.toByteArray();
-  }
+	return Write(colors, w, h, UncompressedWrite, opts and opts.yfunc)
+end
 
-  private static void writeUncompressedDeflateBlock(DataOutputStream zlib, boolean last,
-                        byte[] raw, int off, char len) throws IOException
-  {
-    zlib.writeByte((byte)(last ? 1 : 0));         // Final flag, Compression type 0
-    zlib.writeByte((byte)(len & 0xFF));           // Length LSB
-    zlib.writeByte((byte)((len & 0xFF00) >> 8));  // Length MSB
-    zlib.writeByte((byte)(~len & 0xFF));          // Length 1st complement LSB
-    zlib.writeByte((byte)((~len & 0xFF00) >> 8)); // Length 1st complement MSB 
-    zlib.write(raw,off,len);                      // Data    
-  }
-  
-  private static int calcADLER32(byte[] raw)
-  {
-    int s1 = 1;
-    int s2 = 0;
-    for (int i = 0; i < raw.length; i++)
-    {
-      int abs = raw[i] >=0 ? raw[i] : (raw[i] + 256);
-      s1 = (s1 + abs) % 65521;
-      s2 = (s2 + s1) % 65521;      
-    }
-    return (s2 << 16) + s1;
-  }
-}
-]]
+--- DOCME
+function M.ToString_RGBA (r, g, b, a, w, opts)
+	local ncolors = min(#r, #g, #b, #a)
+	local h, data = ceil(ncolors / w), {}
+	local si, di, extra = 1, 1, w * h - ncolors
+	local yfunc = (opts and opts.yfunc) or DefYieldFunc()
+
+	-- Interleave color streams, inject filters, and do a standard write.
+	repeat
+		data[di], di = 0, di + 1 -- No filter
+
+		for _ = 1, min(w, ncolors) do
+			data[di], data[di + 1], data[di + 2], data[di + 3] = r[si], g[si], b[si], a[si]
+			si, di, ncolors = si + 1, di + 4, ncolors - 1
+		end
+
+		yfunc()
+	until ncolors == 0
+
+	for _ = 1, extra do
+		data[di], data[di + 1], data[di + 2], data[di + 3] = 0, 0, 0, 0
+		di = di + 4
+	end
+
+	yfunc()
+
+	return Write(data, w, h, UncompressedWrite, opts and opts.yfunc)
+end
+
+-- Cache module members.
+_ToString_Interleaved_ = M.ToString_Interleaved
+_ToString_RGBA_ = M.ToString_RGBA
 
 -- Export the module.
 return M
