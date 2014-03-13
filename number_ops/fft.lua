c345cb1e39f9f7723a3d57391048af4be7cc1869
Steven Johnson <steve@xibalbastudios.com>
2014-03-13 17:52:46 -0600

    Minor simplifications

--- number_ops/fft.lua
+++ number_ops/fft.lua
@@ -37,7 +37,7 @@ local M = {}
 local function BitReverse (v, n)
 	local j = 0
 
-	for i = 0, 2 * n - 4, 2 do
+	for i = 0, n + n - 4, 2 do
 		if i < j then
 			v[i + 1], v[i + 2], v[j + 1], v[j + 2] = v[j + 1], v[j + 2], v[i + 1], v[i + 2]
 		end
@@ -53,19 +53,17 @@ local function BitReverse (v, n)
 end
 
 --
-local function Transform (v, n, angle)
+local function Transform (v, n, theta)
 	if n <= 1 then
 		return
 	end
 
 	BitReverse(v, n)
 
-	local dual = 1
+	local n2, dual, dual2, dual4 = n + n, 1, 2, 4
 
 	repeat
-		local dual2 = 2 * dual
-
-		for i = 1, 2 * n - 1, 2 * dual2 do
+		for i = 1, n2 - 1, dual4 do
 			local j = i + dual2
 			local ir, ii = v[i], v[i + 1]
 			local jr, ji = v[j], v[j + 1]
@@ -74,14 +72,13 @@ local function Transform (v, n, angle)
 			v[i], v[i + 1] = ir + jr, ii + ji
 		end
 
-		local theta = angle / dual
 		local s, s2 = sin(theta), 2.0 * sin(theta * 0.5)^2
 		local wr, wi = 1.0, 0.0
 
 		for a = 3, dual2 - 1, 2 do
 			wr, wi = wr - s * wi - s2 * wr, wi + s * wr - s2 * wi
 
-			for i = a, a + 2 * (n - dual2), 2 * dual2 do
+			for i = a, a + n2 - dual4, dual4 do
 				local j = i + dual2
 				local jr, ji = v[j], v[j + 1]
 				local dr, di = wr * jr - wi * ji, wr * ji + wi * jr
@@ -92,16 +89,20 @@ local function Transform (v, n, angle)
 			end
 		end
 
-		dual = dual2
+		dual, dual2, dual4, theta = dual2, dual4, dual4 + dual4, .5 * theta
 	until dual >= n
 end
 
 --- DOCME
+-- @array v
+-- @uint n
 function M.FFT (v, n)
 	Transform(v, n, pi)
 end
 
 --- DOCME
+-- @array v
+-- @uint n
 function M.FFT_Real (v, n)
 	-- stuff
 end
@@ -109,16 +110,26 @@ end
 -- TODO: Two FFT's? (SeparateRealResults does some of it...)
 
 --- DOCME
+-- @array v
+-- @uint n
 function M.IFFT (v, n)
 	Transform(v, n, -pi)
 end
 
 --- DOCME
+-- @array v
+-- @uint n
 function M.IFFT_Real (v, n)
 	-- stuff
 end
 
 --- DOCME
+-- @array out
+-- @uint size
+-- @array arr1
+-- @uint m
+-- @array arr2
+-- @uint n
 function M.PrepareTwoRealSets (out, size, arr1, m, arr2, n)
 	if m > n then
 		arr1, arr2, m, n = arr2, arr1, n, m
@@ -140,6 +151,9 @@ function M.PrepareTwoRealSets (out, size, arr1, m, arr2, n)
 end
 
 --- DOCME
+-- @array v
+-- @uint n
+-- @array out?
 function M.SeparateRealResults_Mul (v, n, out)
 	out = out or v
 
@@ -153,9 +167,9 @@ function M.SeparateRealResults_Mul (v, n, out)
 	for i = 3, n, 2 do
 		local j = len - i
 		local r1, i1, r2, i2 = v[i], v[i + 1], v[j], v[j + 1]
-		local a, b = .5 * (r1 + r2), .5 * (i1 - i2) 
-		local c, d = .5 * (i1 + i2), .5 * (r2 - r1)
-		local real, imag = a * c - b * d, b * c + a * d
+		local a, b = r1 + r2, i1 - i2 
+		local c, d = i1 + i2, r2 - r1
+		local real, imag = .25 * (a * c - b * d), .25 * (b * c + a * d)
 
 		out[i], out[i + 1] = real, imag
 		out[j], out[j + 1] = real, -imag
