0107e9196d6b4ab256157e902edf44ae339b841d
Steven Johnson <steve@xibalbastudios.com>
2014-06-06 02:43:05 -0500

    Some ostensibly functional conjugate gradient code, also working on getting the preconditioner working (maybe getting a matrix lib kicked off?)

--- number_ops/conjugate_gradient.lua
+++ number_ops/conjugate_gradient.lua
@@ -26,5 +26,139 @@
 -- Exports --
 local M = {}
 
+--
+local function Dot (v, w, n)
+	local sum = v[1] * w[1]
+
+	for i = 2, n do
+		sum = sum + v[i] * w[i]
+	end
+
+	return sum
+end
+
+-- --
+local Ax = {}
+
+--
+local function MatrixTimesVector (A, x, n)
+	local j = 1
+
+	for i = 1, n do
+		local sum = 0
+		
+		for k = 1, n do
+			sum, j = sum + A[j] * x[k], j + 1
+		end
+
+		Ax[i] = sum
+	end
+end
+
+-- --
+local R, P = {}, {}
+
+--- DOCME
+-- @array out
+-- @array A
+-- @array b
+-- @uint n
+-- @array[opt] x0
+function M.ConjugateGradient (out, A, b, n, x0)
+	-- Compute initial residual.
+	local rk2 = 0
+
+	if x0 then
+		MatrixTimesVector(A, x0, n)
+
+		for i = 1, n do
+			local ri = b[i] - Ax[i]
+
+			R[i], P[i], rk2, out[i] = ri, ri, rk2 + ri^2, x0[i]
+		end
+	else
+		for i = 1, n do
+			local ri = b[i]
+
+			R[i], P[i], rk2, out[i] = ri, ri, rk2 + ri^2, 0
+		end
+	end
+
+	--
+	while true do
+		MatrixTimesVector(A, P, n)
+
+		--
+		local alpha, rnext = rk2 / Dot(P, Ax, n), 0
+
+		for i = 1, n do
+			out[i] = out[i] + alpha * P[i]
+
+			local ri = R[i] - alpha * Ax[i]
+
+			R[i], rnext = ri, rnext + ri^2
+		end
+
+		--
+		if rnext < 1e-3 then
+			break
+		end
+
+		--
+		local beta = rnext / rk2
+
+		for i = 1, n do
+			P[i] = R[i] + beta * P[i]
+		end
+
+		rk2 = rnext
+	end
+end
+
+-- --
+local Z = {}
+
+-- A 0 0 | A B D | x1 = z1
+-- B C 0 | 0 C B | x2 = z2
+-- D E F | 0 0 F | x3 = z3
+
+-- a 0 0 | A 0 0 = 1 0 0 -> a * A = 1 -> a = ****1 / A****
+-- d e 0 | B C 0 = 0 1 0 -> d * A + e * B = 0, e * C = 1, e = ****1 / C**** -> d * A = -B / C -> d = *****-B / (A * C)*****
+-- g h i | D E F = 0 0 1 -> g * A + h * B + i * D = 0 -> i = *****1 / F*****, h = *****-E / (C * F)*****
+
+-- Row i:
+-- Solve diagonal = 1 / L[i,i]
+-- Going left, solve for column k: sum over j in [k + 1, i]: inverse[i,j] * L[j,i], then inverse[i,k] * L[k,i] + sum = 0 -> inverse[i,k] = -sum / L[k,i]
+
+-- (Looks a lot like the decomp...)
+
+--- DOCME
+-- @array out
+-- @array MI
+-- @array A
+-- @array b
+-- @uint n
+-- @array[opt] x0
+function M.ConjugateGradient_Precond (out, MI, A, b, n, x0)
+--[[
+	r[0] = b - X * x[0]
+	z[0] = inverse(M) * r[0] (or M1 * M2 * z[0] = r[0]...)
+	p[0] = z[0]
+	k = 0
+	while true do
+		a[k] = (transpose(r[k]) * z[k]) / (transpose(p[k]) * A * p[k])
+		x[k+1] = x[k] + alpha[k] * p[k]
+		r[k+1] = r[k] - alpha[k] * A * p[k]
+		if IsSmall(r[k+1]) then
+			break
+		end
+		z[k+1] = inverse(M) * r[k+1]
+		beta[k] = (tranpose(z[k+1]) * r[k+1]) / (transpose(z[k]) * r[k])
+		p[k+1]=z[k+1] + beta[k] * p[k]
+		k = k + 1
+	end
+]]
+end
+
 -- Export the module.
 return M
--- samples/TESTING.lua
+++ samples/TESTING.lua
@@ -37,66 +37,20 @@ end
 Scene:addEventListener("create")
 
 --
-function Scene:show ()
+function Scene:show (e)
+if e.phase == "did" then return end
+local cg = require("number_ops.conjugate_gradient")
+
+	local X = {}
+
+	cg.ConjugateGradient(X, { 4, 1, 1, 3 }, { 1, 2 }, 2, { 2, 1 })
+print(1/11,7/11)
+	vdump(X)
 --[=[
-	Stub code from Wikipedia:
-
-	-- Normal:
-	r[0] = b - A * x[0]
-	p[0] = r[0]
-	k = 0
-	while true do
-		a[k] = (transpose(r[k]) * r[k]) / (transpose(p[k]) * A * p[k])
-		x[k+1] = x[k] + alpha[k] * p[k]
-		r[k+1] = r[k] - alpha[k] * A * p[k]
-		if IsSmall(r[k+1]) then
-			break
-		end
-		beta[k] = (tranpose(r[k+1]) * r[k+1]) / (transpose(r[k]) * r[k])
-		p[k+1]=r[k+1] + beta[k] * p[k]
-		k = k + 1
-	end
-	-- Result = x[k+1]
-
 	--[[
 					 [4 1][x1] = [1]		[2]		[1/11]
 		Example Ax = [1 3][x2] = [2], x0 = 	[1], x =[7/11]
 	]]
-
-	-- Preconditioned:
-	r[0] = b - X * x[0]
-	z[0] = inverse(M) * r[0]
-	p[0] = z[0]
-	k = 0
-	while true do
-		a[k] = (transpose(r[k]) * z[k]) / (transpose(p[k]) * A * p[k])
-		x[k+1] = x[k] + alpha[k] * p[k]
-		r[k+1] = r[k] - alpha[k] * A * p[k]
-		if IsSmall(r[k+1]) then
-			break
-		end
-		z[k+1] = inverse(M) * r[k+1]
-		beta[k] = (tranpose(z[k+1]) * r[k+1]) / (transpose(z[k]) * r[k])
-		p[k+1]=z[k+1] + beta[k] * p[k]
-		k = k + 1
-	end
-	-- Result = x[k+1]
-
-	-- Cholesky, rank-one update (matlab):
-	--[[
-	function [L] = cholupdate(L,x)
-    p = length(x);
-    x = x'
-    for k=1:p
-        r = sqrt(L(k,k)^2 + x(k)^2);
-        c = r / L(k, k);
-        s = x(k) / L(k, k);
-        L(k, k) = r;
-        L(k,k+1:p) = (L(k,k+1:p) + s*x(k+1:p)) / c;
-        x(k+1:p) = c*x(k+1:p) - s*L(k, k+1:p);
-    end
-	]]
-	-- Downdate: "replace the two additions in the assignment to r and L(k,k+1:p) by subtractions"
 --]=]
 end
 
