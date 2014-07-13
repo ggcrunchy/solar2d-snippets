f948d195a2b52bc868a187a83d3a6a40b387e35f
Steven Johnson <steve@xibalbastudios.com>
2014-07-13 18:02:41 -0500

    Amazingly, only one transcription error of the colored corners table, now fixed

--- samples/ColoredCorners.lua
+++ samples/ColoredCorners.lua
@@ -45,18 +45,31 @@ end
 
 Scene:addEventListener("create")
 
--- --
+--[[
+	From "An Alternative for Wang Tiles: Colored Edges versus Colored Corners":
+
+	"With the backtracking algorithm, we are able to compute Wang and corner tile packings for
+	two, three, and four colors. A solution for C colors can often be found more quickly by
+	starting from a solution of C − 1 colors. This way, a recursive tile packing is obtained.
+	A recursive corner tile packing for four colors is shown in Figure 9. Some of these tile
+	packings took almost one year of CPU time to compute on a cluster with 400 2.4 GHz CPUs.
+	More solutions and a description of the implementation of our parallel backtracking
+	algorithm can be found in Lagae and Dutré [2006b]."
+
+	Corner weights:
+
+	1 --- 16
+	|      |
+	2 ---  4
+
+	To obtain the numeric tile values shown in the paper, the numeric values associated with a
+	given tile's colors are multiplied by the corresponding corner weights and summed.
+]]
+
+-- Numeric values of red, yellow, green, blue --
 local R, Y, G, B = 0, 1, 2, 3
 
--- --
--- From the paper:
--- "With the backtracking algorithm, we are able to compute Wang and corner tile packings for
--- two, three, and four colors. A solution for C colors can often be found more quickly by
--- starting from a solution of C − 1 colors. This way, a recursive tile packing is obtained.
--- A recursive corner tile packing for four colors is shown in Figure 9. Some of these tile
--- packings took almost one year of CPU time to compute on a cluster with 400 2.4 GHz CPUs.
--- More solutions and a description of the implementation of our parallel backtracking
--- algorithm can be found in Lagae and Dutré [2006b]."
+-- Recursive tile packing --
 local Colors = {
 	R, R, Y, R, R, G, R, G, G, R, B, B, R, B, R, B, -- N.B. Last row, column wrap
 	G, B, G, B, Y, B, B, Y, B, G, B, G, Y, G, B, B,
@@ -72,7 +85,7 @@ local Colors = {
 	G, Y, G, Y, G, Y, G, R, G, G, R, R, Y, Y, R, G,
 	R, R, Y, R, R, G, R, G, R, R, B, G, B, Y, B, B,
 	Y, R, Y, Y, Y, R, Y, Y, G, Y, Y, R, R, Y, R, Y,
-	R, Y, Y, Y, R, G, R, G, G, R, B, Y, B, B, R, B,
+	R, Y, Y, Y, R, G, R, G, G, R, B, Y, B, R, B, B,
 	R, R, R, Y, R, R, R, Y, Y, R, R, R, R, Y, R, G
 }
 
@@ -81,61 +94,16 @@ function Scene:show (event)
 	if event.phase == "did" then
 		-- Something to load pictures (pretty much available in seams sample)
 		-- Pick energy function? (Add one or both from paper)
-		-- State to hold seam nodes (basically, try to recreate the example in the paper...)
-		-- Choice of algorithms: random, sub-patch, whole patch (seems correlation would be useful, here)
-		-- Way to tune the randomness (k = .001 to 1, as in the GC paper, say)
+		-- Way to tune the randomness? (k = .001 to 1, as in the GC paper, say)
+		-- Choose 2, 3, 4 colors (n = num_colors^2)
+		-- Choose dimension per tile (up to max texture size / 2^num_colors)
+		-- Feathering / multiresolution splining options
 		-- Way to fire off the algorithm
 
-		local index, y, x = 1, 25
-
-		for row = 1, 17 do
-			if row == 17 then
-				index = 1
-			end
-
-			x = 300
-
-			local nums = {}
-
-			for col = 1, 17 do
-				local color
-
-				if col == 17 then
-					color = Colors[index - 16]
-				else
-					color = Colors[index]
-
-					if row < 17 then
-						local below = row < 16 and index + 16 or col
-						local right = col < 16 and 1 or -15
-
-						nums[#nums + 1] = tostring(color + 4 * Colors[below] + 16 * Colors[below + right] + 64 * Colors[index + right])
-					end
-
-					index = index + 1
-				end
-
-				local circ = display.newCircle(self.view, x, y, 8)
-
-				if color == R then
-					circ:setFillColor(.6, 0, 0)
-				elseif color == G then
-					circ:setFillColor(0, .6, 0)
-				elseif color == B then
-					circ:setFillColor(0, 0, .6)
-				elseif color == Y then
-					circ:setFillColor(.8, .8, 0)
-				end
-
-				x = x + 20
-			end
-
-			if row < 17 then
-				print(table.concat(nums, ", "))
-			end
-
-			y = y + 20
-		end
+		-- Step 1: Choose all the stuff (files, num colors, size)
+		-- Step 2: Find the color patches
+		-- Step 3: For i = 1, n do (probably work from lower-left out, to accommodate each size)
+		-- 	 Solve patch
 	end
 end
 
